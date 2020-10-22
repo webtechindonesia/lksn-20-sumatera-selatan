95879c5c81a37d0fc61e8447c03e205a82b7b72e
rahmanfajri <rahmanfajri.rf@gmail.com>
2020-10-22 10:33:23 +0700

    client_side

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Phytons Game</title>
+</head>
+<style type="text/css">
+	.screen{
+		width: 1024px;
+		height: 1024px;
+		background-image: linear-gradient(to bottom right, #002f3d, #414242, #002f3d) ;
+		margin : 0 auto;
+		border-radius: 20px;
+	}
+	.play{
+		padding-top: 20px;
+		padding-left: 38%;
+		width: 200px;
+		height: 200px;
+	}
+
+</style>
+<body>
+	<div class="screen">
+		<img src="design/Sumsel_logo.png" style=" padding-top: 20px; padding-left: 40%;"><br><br>
+		<input type="text" name="nama"><br>
+		<div id="inst"><a href=""><button type="Submit">Instruction</button></a></div><br>
+		<a href="game.html"><img class="play" src="design/Sumsel_Play.png"></a>
+
+	</div>
+</body>
+</html>
